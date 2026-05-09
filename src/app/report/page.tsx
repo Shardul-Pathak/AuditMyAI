20846062464a07965ff0036eb520c0a46a8f91a3
Shardul-Pathak <shardulpathak796@gmail.com>
2026-05-09 17:06:57 +0530

    feat: add report template

---
+++ src/app/report/page.tsx
@@ -0,0 +1,9 @@
+import ReportPdf from "@/components/report/ReportPdf";
+
+export default function ReportPage() {
+  return (
+    <div className="w-50% h-full">
+      <ReportPdf />
+    </div>
+  );
+}
